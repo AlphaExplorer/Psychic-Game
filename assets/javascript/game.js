d0fdc7b28d8dbb358813f4236a7fc871f8da733c
Francisco Gonzalez <francisco@primestaram.com>
2019-06-27 16:20:25 -0400

    Latest Update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,24 @@ var userKeyStokes = []
 var characterNameList = ["JonhSnow", "Daenerys"] 
 var numberWins = 0 
 var maxNumberGuesses = 12
+var guessingBoardSymbol = "-"
+var guessingBoard = []
 
-
-// Random selection of a character to initiate the game
+// Select randomly a character to play the game
 var randoumCharacter = characterNameList[Math.floor(Math.random()*characterNameList.length)];
 console.log(randoumCharacter)
 
-
+// Generate Guessing Board 
+var numberCharactersInName = randoumCharacter.length;
+console.log(numberCharactersInName);
+var boardGenerator=function(arrayLength)
+{
+  for(var i=0; i<arrayLength; i++)
+  {
+    $("#gessingBoard").append('<div id="number'+i+'">'+guessingBoardSymbol+'<div>');
+  }
+} 
+boardGenerator(numberCharactersInName);
 
 // Capture the user key strokes and store only unique keysrokes into userKeyStrokes array
 document.onkeyup = function(){
@@ -21,4 +32,33 @@ document.onkeyup = function(){
       }
 }
 
-document.write(randoumCharacter)
+// var BoardUpdater = function()
+// {
+
+// }
+
+// // var arr1 = ["abc"];
+// // var arr2 = ["a", "b", "d"];
+
+// // var comparingFunction = function(str, letter)
+// // {
+// //     var indices = [];
+// //     for(var i=0; i<str.length;i++) {
+// //         if (str[i] === toString(letter)) indices.push(i);
+// //     }
+// // }
+// // console.log(comparingFunction(arr1, "b"));
+
+
+var str = "scissors";
+
+
+function blank(str) {
+  var indices = [];
+  for(var i=0; i<str.length;i++) {
+    if (str[i] === "s") indices.push(i);
+    return indices
+}
+}
+
+console.log(blank(str))
