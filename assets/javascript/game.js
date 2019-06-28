facf9fe2a80132e062b6208010cb1acf2dfba0dc
Francisco Gonzalez <francisco@primestaram.com>
2019-06-28 18:00:22 -0400

    Latest updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,39 @@
 // Variable declaration 
-var userKeyStokes = []  
-var characterNameList = ["JonhSnow", "Daenerys"] 
-var numberWins = 0 
-var maxNumberGuesses = 12
-var guessingBoardSymbol = "-"
-var guessingBoard = []
+var characterNameList = ["Jonh Snow", "Daenerys"];
+var numberWins = 0; 
+var maxNumberGuesses = 12;
+var guessingBoardSymbol = "_";
+var guessingBoard = [];
 
 // Select randomly a character to play the game
-var randoumCharacter = characterNameList[Math.floor(Math.random()*characterNameList.length)];
+var randoumCharacter = characterNameList[Math.floor(Math.random()*characterNameList.length)].toLowerCase().replace(/ /g, '');
+// Notice that we are making all the characters in the name lower case and we are eliminating the empty spaces
 console.log(randoumCharacter)
 
 // Generate Guessing Board 
-var numberCharactersInName = randoumCharacter.length;
-console.log(numberCharactersInName);
-var boardGenerator=function(arrayLength)
+var numberCharactersInName = randoumCharacter.length; //store the number of characters in the name of the random character to create the dashes on the guessing board
+console.log(numberCharactersInName)
+var boardGenerator=function(arrayLength)//create a formula the creates automatically the number of dashes required for the guessing board
 {
   for(var i=0; i<arrayLength; i++)
   {
-    $("#gessingBoard").append('<div id="number'+i+'">'+guessingBoardSymbol+'<div>');
+    $("#name_board").append('<div class="dash" id="'+randoumCharacter[i]+'">'+guessingBoardSymbol+'<div>');
   }
 } 
 boardGenerator(numberCharactersInName);
 
-// Capture the user key strokes and store only unique keysrokes into userKeyStrokes array
-document.onkeyup = function(){
-    var userguess = String.fromCharCode(event.keyCode).toLocaleLowerCase();
-    console.log(userguess);
-    if(userKeyStokes.indexOf(userguess) === -1) {
-        userKeyStokes.push(userguess);
-        console.log(userKeyStokes);
-      }
+//Display & Capture user keystrokes
+var listOfKeyPressed = [] //create an empty array where to store all the pressed keys by the user
+document.onkeyup = function keyStroke (event){
+  var last_pressed_key = event.key;
+  if(listOfKeyPressed.indexOf(last_pressed_key) === -1) 
+  {
+    listOfKeyPressed.push(last_pressed_key);
+    console.log(listOfKeyPressed);
+  }
+ 
+  document.getElementById("last_pressed_key").innerHTML = "Your last pressed key was: " + last_pressed_key;
+  document.getElementById("letters_guessed").innerHTML = "You have pressed so far the following letter: " + listOfKeyPressed;
 }
 
-// var BoardUpdater = function()
-// {
 
-// }
-
-// // var arr1 = ["abc"];
-// // var arr2 = ["a", "b", "d"];
-
-// // var comparingFunction = function(str, letter)
-// // {
-// //     var indices = [];
-// //     for(var i=0; i<str.length;i++) {
-// //         if (str[i] === toString(letter)) indices.push(i);
-// //     }
-// // }
-// // console.log(comparingFunction(arr1, "b"));
-
-
-var str = "scissors";
-
-
-function blank(str) {
-  var indices = [];
-  for(var i=0; i<str.length;i++) {
-    if (str[i] === "s") indices.push(i);
-    return indices
-}
-}
-
-console.log(blank(str))
